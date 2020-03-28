73ce6782defb4c51edaee450b05bc06ebbfccbf7
Wompipomp <markaltmann@googlemail.com>
2020-03-28 21:46:25 +0100

    fix java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,15 @@ plugins {
 
 group = "com.satellytes"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
